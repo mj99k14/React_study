7e6aa4cb5da3c8ff63f85f40bac88b4f02ca1f79
kimminjung <ekfkawnl644@gmail.com>
2026-04-16 20:24:57 +0900

    Expenseltem 컴포넌트 생성하기

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
-
+import "./ExpenseItem.css"
 export class ExpenseItem extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <li className='item'>
+        <div className='info'>
+          <span className='expense'>학원등록</span>
+          <span className='amount'>원</span>
+        </div>
+        <div>
+          <button className='edit-btn'>수정</button>
+          <button className='clear-btn'>삭제</button>
+        </div>
+
+      </li>
     )
   }
 }
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import "./ExpenseList.css"
+import ExpenseItem from './ExpenseItem'
 export class ExpenseList extends Component {
   render() {
     return (
       <div>
         <ul className='list'>
           {}
+          <ExpenseItem/>
         </ul>
         <button className='btn'>
           목록 지우기
