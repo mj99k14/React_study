7d7cf587a1d4829d1c69ad20fe9c8c4234109849
kimminjung <ekfkawnl644@gmail.com>
2026-04-20 21:49:24 +0900

    패스트캠퍼스 9-7 alert생성하기

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import React from 'react'
 import "./App.css";
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from "./components/ExpenseList";
+import Alert from "./components/Alert";
 const App = () => {
   const [charge,setCharge] = useState("");
   const [amount,setAmount] = useState(0);
-  
+
+  const [alert, setAlert] = useState({show: false});
+
   const [expenses,setExpenses] = useState(
 [
       {id: 1, charge:"렌트비", amount: 1600},
@@ -30,8 +33,17 @@ const App = () => {
     const newexpenses = expenses.filter(expense => expense.id !== id);
     console.log(newexpenses);
     setExpenses(newexpenses);
+    handleAlert({type:'danger', text:'아이템이 삭제되었습니다.'});
   }
   
+  const handleAlert =({type,text}) =>{
+    setAlert({show: true, type, text});
+    setTimeout(() =>{
+      setAlert({show: false});
+    },7000);
+
+  }
+
   const handleSubmit = (e) =>{
     e.preventDefault();
     if (charge !=="" && amount > 0){
@@ -41,16 +53,20 @@ const App = () => {
       setExpenses(newExpenses);
       setCharge("");
       setAmount(0);
-    
-    
+      handleAlert({type:"success", text:"아이템이 생성되었습니다."});
     }else{
       console.log('error');
+      handleAlert({
+        type: 'danger',
+        text: 'charge는 빈 값일 수 없으며 amount는 0보다 커야 합니다.'
+      })
     }
 
     }
 
     return(
       <main className="main-container">
+        {alert.show ? <Alert type={alert.type} text={alert.text}/>: null}
         <h1>예산 계산기</h1>
         
         <div style={{width:'100%',backgroundColor: 'white',padding: '1rem'}}>
